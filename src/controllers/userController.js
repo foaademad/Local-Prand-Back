3a6065d5aa989d4f035736191dc3ed94659e22a2
foaud Emad <foaademad66@gmail.com>
2025-09-21 23:38:07 +0300

    make admin role

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 // User registration
 let register = async (req, res) => {
     try {
-    const { name, email, password, confirmPassword, phone, address } = req.body;
+    const { name, email, password, confirmPassword, phone, address, role } = req.body;
 
         if (password !== confirmPassword) {
             return res.status(400).json({ message: "Passwords do not match" });
@@ -21,7 +21,8 @@ let register = async (req, res) => {
             email,
             password,
             phone,
-            address
+            address,
+            role
         });
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '3h' });
